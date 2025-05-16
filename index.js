2ea9e4b6418f661a30994847221f7615649a4059
Simon <ogbanjesi@gmail.com>
2025-05-16 13:43:15 +0100

    Some Modification on package.jsn and comments on index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+// the 1st step is yo Import the required modules. THis is because This is Node.js's built-in HTTP module 
+// that allows you to create HTTP servers and make HTTP requests.
+import http, { Server } from 'http';
+import fs from 'fs';
+
+// You need to difine the port number on which your server will listen for incoming requests.
+// The most common Port number are 3000, 8000, 8080. So I choose 8080
+const PORT = 8080;
+
+// Create the HTTP server
+const server = http.createServer((req, res) => {
+    // Handle incoming requests
+    if (req.url === '/') {
+        // Read the HTML file
+        fs.readFile('index.html', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+                return;
+            }
+
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+    }
+});
+
+// Start the server
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT} at http://localhost:${PORT}`);
+});
