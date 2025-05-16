cb919b6b49260ce00ee4179a18854f6ee56181ca
Simon <ogbanjesi@gmail.com>
2025-05-16 14:19:20 +0100

    added comment on index.js

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-// the 1st step is yo Import the required modules. THis is because This is Node.js's built-in HTTP module 
+// the 1st step is yo Import the required modules. THis is because Node.js's built-in HTTP module 
 // that allows you to create HTTP servers and make HTTP requests.
 import http, { Server } from 'http';
 import fs from 'fs';
 
-// You need to difine the port number on which your server will listen for incoming requests.
+// Step 2: You need to difine the port number on which your server will listen for incoming requests.
 // The most common Port number are 3000, 8000, 8080. So I choose 8080
 const PORT = 8080;
 
-// Create the HTTP server
+// Step 3: Create the HTTP server
+//http.createServer() creates a new HTTP server.
+//It takes a callback function that will be executed for every incoming request.
+//The callback receives two objects:
+//req (Request): Contains information about the incoming request (URL, headers, etc.)
+//res (Response): Used to send data back to the user
 const server = http.createServer((req, res) => {
     // Handle incoming requests
     if (req.url === '/') {
